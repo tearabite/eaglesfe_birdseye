3c669aba7096e3af7fdff1c381b06e56efd85138
Scott Barnes <tearabite@gmail.com>
2018-10-24 23:56:57 -0500

    Moved config pane dom into actual html so it can be enhanced more easily

--- client/scripts/telemetry.js
+++ client/scripts/telemetry.js
@@ -1,55 +1,73 @@
-var hud;
-var log;
-var cmd;
+var hud, log, cmd;
 
-(function buildRightRail() {
-    let rightRail = document.createElement('div');
-    rightRail.id = 'rightRail';
-    document.body.appendChild(rightRail);
+hud = CodeMirror(inner.querySelector('#hud'), {
+    lineWrapping: true
+});
+log = CodeMirror(inner.querySelector('#log'), {
+    lineWrapping: true
+});
 
-    let inner = document.createElement('div');
-    inner.id = 'inner';
-    rightRail.appendChild(inner);
+var clog = console.log;
+console.error = console.warn = console.info = console.debug = console.log = (message) => {
+    clog(message);
+    log.setValue(log.getValue() + '\n' + message);
+    log.execCommand('goDocEnd');
+}
 
-    let hud = document.createElement('div');
-    hud.id = 'hud';
-    inner.appendChild(hud);
-    hud = CodeMirror(inner.querySelector('#hud'), {lineWrapping: true });
+cmd = rightRail.querySelector('#cmd');
+cmd.addEventListener('change', (e) => {
+    ws.send(JSON.stringify(cmd.value));
+    cmd.value = '';
+});
 
-    let log = document.createElement('div');
-    log.id = 'log';
-    inner.appendChild(log);
-    log = CodeMirror(inner.querySelector('#log'), { lineWrapping: true });
-
-    var clog = console.log;
-    console.error = console.warn = console.info = console.debug = console.log = (message) => {
-        clog(message);
-        log.setValue(log.getValue() + '\n' + message);
-        log.execCommand('goDocEnd');
-    }
-
-    let cmd = document.createElement('input');
-    cmd.id = 'cmd';
-    cmd.type = 'text';
-    rightRail.appendChild(cmd);
-    cmd = rightRail.querySelector('#cmd');
-    cmd.addEventListener('change', (e) => {
-        ws.send(JSON.stringify(cmd.value));
-        cmd.value = '';
-    });
-
-    document.addEventListener('mouseover', (e) => {
-        if (controls !== undefined) {
-            controls.enabled = e.path[0].tagName === 'CANVAS';
+popIn = () => {
+    rightRail.animate([{
+            opacity: 0,
+            transform: 'scale(0.7)',
+        },
+        {
+            transform: 'scale(1.2)',
+            opacity: 1,
+            easing: 'ease-out'
+        },
+        {
+            transform: 'scale(1.0)',
+            opacity: 1,
+            easing: 'ease-in'
         }
-    });
+    ], 350);
+};
 
-    document.addEventListener('telemetryUpdated', (e) => {
-        hud.setValue(JSON.stringify(telemetry, null, 2));
-    });
+popOut = () => {
+    rightRail.animate([{
+            opacity: 1,
+            transform: 'scale(1.0)',
+        },
+        {
+            transform: 'scale(1.2)',
+            opacity: 1,
+            easing: 'ease-out'
+        },
+        {
+            transform: 'scale(0.7)',
+            opacity: 0,
+            easing: 'ease-in'
+        }
+    ], 350);
+};
 
-    document.addEventListener('connected', (e) => {
-        log.log(`Connection esablished to ${args.address} on port ${args.port}!`);
-    });
-})();
+document.addEventListener('mouseover', (e) => {
+    if (controls !== undefined) {
+        controls.enabled = e.path[0].tagName === 'CANVAS';
+    }
+});
 
+document.addEventListener('telemetryUpdated', (e) => {
+    hud.setValue(JSON.stringify(telemetry, null, 2));
+});
+
+document.addEventListener('connected', (e) => {
+    console.log(`Connection esablished to ${args.address} on port ${args.port}!`);
+});
+
+popIn();
