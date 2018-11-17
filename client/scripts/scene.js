65c3e8f6302920fa7ee54fcb8e58426e78adcf2d
Scott Barnes <tearabite@gmail.com>
2018-11-17 03:03:58 -0600

    Added two target pins which visually indicate the robots intended target and the location of that point after we translate it into robot space.
    These use target.field and target.robot in the telemetry payload.

--- client/scripts/scene.js
+++ client/scripts/scene.js
@@ -111,18 +111,70 @@ request.onreadystatechange = (e) => {
     }
 }
 
+// Target Pin
+class TargetPin {
+    constructor(color) {
+        this.color = color;
+        let pinMaterial = new THREE.MeshPhongMaterial({ color: color });
+
+        const coneHeight = 3;
+        const cylinderHeight = 1;
+
+        let cylinder = new THREE.CylinderGeometry(1, 1, cylinderHeight);
+        let cone = new THREE.ConeGeometry(1, coneHeight);
+        cone.applyMatrix(new THREE.Matrix4().makeRotationX(THREE.Math.degToRad(-90)));
+        cylinder.applyMatrix(new THREE.Matrix4().makeRotationX(THREE.Math.degToRad(90)));
+
+        cone.applyMatrix(new THREE.Matrix4().makeTranslation(0,0, coneHeight/2));
+        cylinder.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0, (cylinderHeight / 2) + coneHeight));
+
+        cylinder = new THREE.Mesh(cylinder, pinMaterial);
+        cone = new THREE.Mesh(cone, pinMaterial);
+
+        let pin = new THREE.Group();
+        pin.add(cylinder);
+        pin.add(cone);
+
+        return pin;
+    }
+}
+
+var targetPin = new TargetPin(0xffff00);
+targetPin.visible = false;
+scene.add(targetPin);
+
+var robotTargetPin = new TargetPin(0xffffff);
+robotTargetPin.visible = false;
+robotTargetPin.translateZ(4);
+scene.add(robotTargetPin);
+
 class RobotPlaceholder {
     get dimensions() {
         return { width: 18, height: 18, depth: 18 };
     };
 
     constructor() {
-        this.robotGeometry = new THREE.BoxBufferGeometry(this.dimensions.width, this.dimensions.height, this.dimensions.depth);
-        this.robotGeometry.applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, this.dimensions.depth / 2) );
-        this.robotMaterial = new THREE.MeshPhongMaterial({ color: 0x4080ff, dithering: true });
-        let robot = new THREE.Mesh(this.robotGeometry, this.robotMaterial);
+        let robot = new THREE.Group();
+
+        let cube = new THREE.BoxBufferGeometry(this.dimensions.width, this.dimensions.height, this.dimensions.depth);
+        cube.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0, this.dimensions.depth / 2));
+        robot.add(new THREE.Mesh(
+            cube,
+            new THREE.MeshPhongMaterial({ color: 0x4080ff, dithering: true })
+        ));
+        let cone = new THREE.ConeGeometry(9, 9, 32);
+        cone.applyMatrix(new THREE.Matrix4().makeRotationZ(Math.PI / -2));
+        cone.applyMatrix(new THREE.Matrix4().makeTranslation(this.dimensions.width - 4.5, 0, this.dimensions.depth / 2));
+        robot.add(new THREE.Mesh(
+            cone,
+            new THREE.MeshPhongMaterial({ color: 0xFF0000, dithering: true })
+        ));
+
+        robot.translateZ(new THREE.Matrix4().makeTranslation(0, 0, this.dimensions.depth));
+
         robot.receiveShadow = true;
         robot.castShadow = true;
+        robot.add(new THREE.AxisHelper(50));
         return robot;
     }
 }
@@ -131,28 +183,41 @@ var robot = new RobotPlaceholder();
 robot.position.set(-60, 60, 0);
 scene.add(robot);
 
-// Target Line
-var target = new THREE.Vector3(0, 0, 0);
-var material = new THREE.LineBasicMaterial({ color: 0xff0000 });
-var geometry = new THREE.Geometry();
-geometry.vertices = [new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0)];
-var line = new THREE.Line( geometry, material );
-scene.add(line);
-
 function animate() {
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
 
-    controls.update();
-    if (telemetry !== undefined) {
-        robot.position.set(telemetry.x, telemetry.y, 0);
-        const pitch = THREE.Math.degToRad(telemetry.pitch);
-        const roll = THREE.Math.degToRad(telemetry.roll);
-        const heading = THREE.Math.degToRad(telemetry.heading);
-        robot.rotation.setFromVector3(new THREE.Vector3(roll, pitch, heading));
+    if (telemetry) {
+        const position = telemetry.position;
+        if (position) {
+            robot.position.set(position.x, position.y, 0);
+            const pitch = THREE.Math.degToRad(position.pitch * -1);
+            const roll = THREE.Math.degToRad(position.roll * -1);
+            const heading = THREE.Math.degToRad(position.heading);
+            robot.rotation.setFromVector3(new THREE.Vector3(roll, pitch, heading));
+        }
 
-        line.geometry.vertices[1].setX(telemetry.x);
-        line.geometry.vertices[1].setY(telemetry.y);
-        line.geometry.verticesNeedUpdate = true;
+        const fTarget = telemetry.target && telemetry.target.field;
+        if (fTarget) {
+            const x = fTarget.x || 0;
+            const y = fTarget.y || 0;
+            const z = fTarget.z || 0;
+            targetPin.position.set(x, y, z);
+            targetPin.visible = true;
+        } else {
+            targetPin.visible = false;
+        }
+
+        const rTarget = telemetry.target && telemetry.target.robot;
+        if (fTarget) {
+            const x = rTarget.x || 0;
+            const y = rTarget.y || 0;
+            const z = rTarget.z || 0;
+            const transformed = robot.localToWorld(new THREE.Vector3(x, y, z));
+            robotTargetPin.position.set(transformed.x, transformed.y, 4);
+            robotTargetPin.visible = true;
+        } else {
+            robotTargetPin.visible = false;
+        }
     }
 
 	renderer.render( scene, camera );
